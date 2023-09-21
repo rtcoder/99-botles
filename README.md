6fa8ac051a207ec6c8203fab2e59f95a692a56b9
Dawid Jeż <dawidjez@gmail.com>
2023-09-21 20:33:29 +0200

    readme

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# 99-botles
+# 99 Bottles multilingual
+## Supported languages
+
+|Language    |Code|
+|------------|----|
+|English     |en  |
+|Polish      |pl  |
+|German      |de  |
+|Spanish     |es  |
+
+## Usage 
+```shell
+composer install
+php index.php lang-code
+```
--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
 require_once 'vendor/autoload.php';
-(new Bottles\BottlesGenerator('es'))->generate();
+
+if (!isset($argv[1])) {
+    echo 'Usage: php index.php language' . PHP_EOL
+        . 'Supported languages:' . PHP_EOL
+        . 'en' . PHP_EOL
+        . 'de' . PHP_EOL
+        . 'es' . PHP_EOL
+        . 'pl' . PHP_EOL;
+}
+
+(new Bottles\BottlesGenerator($argv[1]))->generate();
